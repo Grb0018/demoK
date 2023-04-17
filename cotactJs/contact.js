18a9d0b83e9b204a6f515614f33eb6ae6adfde79
Grb0018 <105971445+Grb0018@users.noreply.github.com>
2023-04-17 22:03:20 +0530

    Update contact.js

--- cotactJs/contact.js
+++ cotactJs/contact.js
@@ -1,11 +1,28 @@
 document.getElementById('contactForm').onsubmit=(e)=>{
+    if(document.getElementById('send').innerText=="Send Message"){
     e.preventDefault();
+    document.getElementById('send').innerText="Please Wait .."
     emailjs.sendForm('service_f6kvhe9', 'template_ac7i9yg', '#contactForm')
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
+       document.getElementById('send').innerText="Send Message"
+       const canVibrate = window.navigator.vibrate;
+       if(canVibrate) window.navigator.vibrate(150);
+       document.getElementById('notify').style.display="flex";
+       setTimeout(()=>{
+        document.getElementById('notify').style.display="none";
+        var allInput =document.getElementById('contactForm').querySelectorAll('input');
+         for (const x of allInput) {
+            x.value='';
+        }
+        document.getElementById('contactForm').querySelector('textarea').value='';
+       },2000)
     }, function(error) {
+        alert('Check Your Internet Connection');
+        document.getElementById('send').innerText="Send Message"
        console.log('FAILED...', error);
     });
+}
 }
 var menu=false;
 const menuClick=()=>{
