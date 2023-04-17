354139ba2f1d187213fcb19edc26b65eea725f77
Grb0018 <105971445+Grb0018@users.noreply.github.com>
2023-04-17 15:49:50 +0530

    Update index.js

--- JS-pack/index.js
+++ JS-pack/index.js
@@ -1,7 +1,9 @@
 document.querySelector('.thisPage').style.color='rgb(196, 39, 0)';
 document.querySelector('.thisPage').style.borderBottom='0.2vw solid rgb(255, 69, 7)';
 const whatsappGo = () =>{
-    window.open("https://api.whatsapp.com/send?phone=918420204013&text=Hi%20we%20need%20help%20regarding%20something");
+    var w=window;
+    w.open("https://api.whatsapp.com/send?phone=918420204013&text=Hi%20we%20need%20help%20regarding%20something");
+    setTimeout(()=>{w.close()},1000);
   }
 window.onload=()=>{
 AOS.init();
