f7548be51f262b3a9bfc5106751dff5e67864314
Grb0018 <105971445+Grb0018@users.noreply.github.com>
2023-04-17 15:04:34 +0530

    Delete cotactJs directory

--- cotactJs/contact.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('contactForm').onsubmit=(e)=>{
-    e.preventDefault();
-    emailjs.sendForm('service_f6kvhe9', 'template_ac7i9yg', '#contactForm')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
-}
